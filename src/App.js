70e93326bbb639d346f6908c111485cc452cad3a
Ivan Guzman <n3cr0.2012@gmail.com>
2021-08-02 22:16:40 -0700

    Created tab component with highlights

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+function Tab({ children }) {
+  const [highlightStyle, setHighlightStyle] = useState({ left: 0, opacity: 0 });
+
+  function moveHightLight(e) {
+    //update hightlightStyle to move the highlight
+    setHighlightStyle({
+      left: e.nativeEvent.layerX - 150,
+    });
+  }
+
+  function hideHighlight(e) {
+    setHighlightStyle({
+      opacity: 0,
+      left: e.nativeEvent.layerX - 150,
+    });
+  }
+  return (
+    <div
+      className="tab"
+      onMouseOut={hideHighlight}
+      onMouseMove={moveHightLight}
+    >
+      <div className="highlight" style={highlightStyle} />
+      {children}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="app">
       <div className="browser">
         <div className="tabs">
-          <div className="tab">
+          <Tab>
             <a>Home</a>
-          </div>
-          <div className="tab">
+          </Tab>
+          <Tab>
             <a>About</a>
-          </div>
-          <div className="tab">
+          </Tab>
+          <Tab>
             <a>Features</a>
-          </div>
+          </Tab>
         </div>
 
         <div className="viewport">Pages Go Here</div>
